9f37f5e36294094cd9fe6d37e82efe0cd6bafacb
Erasmus <refugedesigns17@gmail.com>
2023-03-12 22:38:46 +0000

    initial commit - thyson - hotel booking website

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Theme Functions
+ * 
+ * @package thyson
+ */
+
+if(!defined('THYSON_DIR_PATH')) {
+    define('THYSON_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+
+if(!defined('AQUILA_DIR_URI')) {
+    define('AQUILA_DIR_URI', untrailingslashit(get_template_directory_uri()));
+}
